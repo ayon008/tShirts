f7f61118b3e93178370f8972cd9998f527e7ca8c
ayon008 <shariar.ayon128@gmail.com>
2023-10-07 12:35:54 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Components/Home/Home.jsx';
+import Layout from './Components/Layout/Layout.jsx';
+import OrderReview from './Components/OrderReview/OrderReview.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+        loader: () => fetch('tShirt.json')
+      },
+      {
+        path: '/review',
+        element: <OrderReview></OrderReview>
+      },
+      {}
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
